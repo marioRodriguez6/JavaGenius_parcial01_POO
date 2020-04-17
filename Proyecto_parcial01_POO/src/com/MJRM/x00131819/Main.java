fa8045ae6f39bf6c1d6317cbaf6e8f4b3c5414b6
marioR <mariojosue155@gmail.com>
2020-04-17 13:39:31 -0600

    Add: Clases

---
+++ Proyecto_parcial01_POO/src/com/MJRM/x00131819/Main.java
@@ -0,0 +1,8 @@
+package com.MJRM.x00131819;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Proyecto_parcial01_POO/src/com/MJRM/x00131819/ServicioProfesional.java
@@ -0,0 +1,4 @@
+package com.MJRM.x00131819;
+
+public class ServicioProfesional extends Empleado {
+}
